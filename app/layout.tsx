4f08e9c5287b1ec3b7fbc734ca63f5b53e2ab6af
Ujjawal Gupta <ujjawalgupta1999.ug@gmail.com>
2024-04-10 00:09:12 +0530

    Initial clerk setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
 import "./globals.css";
 
@@ -19,9 +20,11 @@ const Layout = ({
   children: ReactNode;
 }>) => {
   return (
-    <html lang="en">
-      <body className={cn("antialiased", inter.className)}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={cn("antialiased", inter.className)}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 };
 
