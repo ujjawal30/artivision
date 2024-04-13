70786f171818963ffa5ec7f10a3a45be7bec8825
Ujjawal Gupta <ujjawalgupta1999.ug@gmail.com>
2024-04-14 04:43:14 +0530

    Created homepage header section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,30 @@
-import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
+import Link from "next/link";
+
+import { navLinks } from "@/constants";
+import Searchbar from "@/components/forms/Searchbar";
 
 const Home = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Homepage
-      <UserButton />
+    <main>
+      <section className="bg-gradient-to-br from-claret-400 to-flamingo-500 sm:flex items-center justify-center hidden h-72 flex-col gap-8 rounded-2xl p-10">
+        <h1 className="text-[40px] font-semibold flex-wrap text-center text-white shadow-sm">
+          Unleash your creativity with Artivision
+        </h1>
+        <ul className="flex items-center justify-center w-full gap-16">
+          {navLinks.slice(1, 5).map((link) => (
+            <Link
+              key={link.label}
+              href={link.route}
+              className="flex items-center justify-center flex-col gap-2"
+            >
+              <li className="flex justify-center items-center rounded-full bg-white p-4">
+                <link.icon size={24} className="text-gray-600" />
+              </li>
+              <p className="font-medium text-center text-white">{link.label}</p>
+            </Link>
+          ))}
+        </ul>
+      </section>
     </main>
   );
 };
